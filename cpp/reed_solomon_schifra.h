71e6bba9dce9c876c2fea3b9fc05ec42c42ca7c9
smarches <skmarches@gmail.com>
2020-12-19 20:04:33 -0500

    some more reed_solomon header cleanup

--- cpp/reed_solomon_schifra.h
+++ cpp/reed_solomon_schifra.h
@@ -17,7 +17,7 @@ using Uchar = std::uint8_t;
 using VecUchar = std::vector<Uchar>;
 
 union b8 {
-	unsigned long long w;
+	std::uint64_t w;
 	unsigned char b[8];
 	b8() {}
 	b8(long long ww) : w(ww) {}
@@ -33,33 +33,34 @@ union b8 {
 	bool operator<=(const b8 &b) { return w <= b.w; }
 };
 
-using namespace schifra;
-
 template<size_t code_length, size_t fec_length>
 class SchifraCode { // see Schifra's example08
     // see schifra_galois_field.hpp
+    // in the block, data and fec are stored contiguously: [data|fec]
     static_assert(code_length > fec_length);
 	static constexpr size_t data_length = code_length - fec_length;
 	static constexpr size_t generator_polynomial_root_count = fec_length;
 	static constexpr int field_descriptor = 8;
 	static constexpr size_t generator_polynomial_index = 120;
-    static constexpr auto pad8 = std::min(8ul,data_length);
-    static constexpr std::array<unsigned,9> pp6{ 1, 1, 1, 0, 0, 0, 0, 1, 1 };
+    // this is necessitated by having method overloads for the b8 type
+    static constexpr auto data_pad = std::min(8ul,data_length);
+    static constexpr auto code_pad = std::min(8ul,code_length);
 
-	using encoder_t = reed_solomon::shortened_encoder<code_length, fec_length, data_length>;
-	using decoder_t = reed_solomon::shortened_decoder<code_length, fec_length, data_length>;
-	using field_t = galois::field;
-	using field_polynomial_t = galois::field_polynomial;
-	using block_t = reed_solomon::block<code_length, fec_length>;
+	using encoder_t = schifra::reed_solomon::shortened_encoder<code_length, fec_length, data_length>;
+	using decoder_t = schifra::reed_solomon::shortened_decoder<code_length, fec_length, data_length>;
+	using field_t = schifra::galois::field;
+	using field_polynomial_t = schifra::galois::field_polynomial;
+    using field_symbol_t = schifra::galois::field_symbol; // just an int!
+	using block_t = schifra::reed_solomon::block<code_length, fec_length>;
 
     // for some reason the default constructors in Schifra are
     // private so we need to declare pointers to the things that will be
-    // initialized, and all the subsequent code is poisoned
+    // initialized, and all the subsequent code is poisoned with dereferences
     field_t *field;
 	encoder_t *encoder;
 	decoder_t *decoder;
 	field_polynomial_t *generator_polynomial;
-	block_t *block; // size of block-> data is template parameter block_length
+	block_t block; // size of block. data is template parameter block_length
     // return value info of decode()
     template<typename T>
     struct rv {
@@ -67,16 +68,18 @@ class SchifraCode { // see Schifra's example08
         bool recoverable;
 		T message;
         rv() = delete;
-        rv(block_t* b) : err_number(b->error),
-            errs_detected(b->errors_detected),
-		    errs_corrected(b->errors_corrected),
-		    recoverable(!b->unrecoverable) {}
+        rv(block_t b,T msg) : err_number(b.error),
+            errs_detected(b.errors_detected),
+		    errs_corrected(b.errors_corrected),
+		    recoverable(!b.unrecoverable) {
+            message = msg;
+        }
     };
     
     void _encode(std::string& data) {
-        auto res = encoder->encode(data, *block);
+        bool res = encoder->encode(data, block);
         if (!res) {
-            std::cerr << "Error - Critical encoding failure!\nMsg: " << block->error_as_string() << '\n';
+            std::cerr << "Error - Critical encoding failure!\nMsg: " << block.error_as_string() << '\n';
             exit(EXIT_FAILURE);
         }
     }
@@ -86,9 +89,13 @@ public:
 	SchifraCode() {
         /* Instantiate Finite Field, Generator Polynomials, Encoder, Decoder */
         // field = new field_t(field_descriptor,pp6.size(), pp6.data());
-        field = new field_t(field_descriptor,pp6.size(), pp6.data());
+        field = new field_t(
+            field_descriptor,
+            schifra::galois::primitive_polynomial_size06,
+            schifra::galois::primitive_polynomial06
+        );
 		generator_polynomial = new field_polynomial_t(*field);
-        auto res = make_sequential_root_generator_polynomial(
+        auto res = schifra::make_sequential_root_generator_polynomial(
             *field,
             generator_polynomial_index,
             generator_polynomial_root_count,
@@ -108,77 +115,75 @@ public:
 
 	VecUchar encode(VecUchar& msg) {
 		std::string message(code_length, 0);
-        std::size_t L = std::min(msg.size(),data_length);
+        std::size_t L = std::min(msg.size(),code_length);
 		for (size_t i = 0; i < L; i++) message[i] = msg[i];
         _encode(message);
 		VecUchar codeword(code_length);
-		for (int i = 0; i < code_length; i++) codeword[i] = static_cast<Uchar>(block->data[i]);
+		for (int i = 0; i < code_length; i++) codeword[i] = static_cast<Uchar>(block.data[i]);
 		return codeword;
 	}
-	b8 encode(b8 msg) {
-		std::string message(data_length,0);
-        // 'msg.b' is exactly 8 bytes
-		for (unsigned i = 0; i < 8; i++) message.push_back(msg.b[i]);
+	b8 encode(const b8 msg) {
         // the encode_t.encode() method blindly copies the input message up to its data_length
         // template parameter regardless of whether the message is actually that long
+        // its not clear what the point of encoding this is, since the fec bytes are not
+        // kept around. It will simply return what was passed in?
+		std::string message(code_length,0);
+        // 'msg.b' is exactly 8 bytes
+		for (unsigned i = 0; i < 8; i++) message[i] = msg.b[i];
+        _encode(message);
 		b8 codeword(0);
-		for (size_t i = 0; i < pad8; i++) codeword.b[i] = static_cast<Uchar>(block->data[i]);
+		for (size_t i = 0; i < data_pad; i++) {
+            codeword.b[i] = static_cast<Uchar>(block.data[i]);
+        }
 		return codeword;
 	}
     
-	rv<VecUchar> decode(VecUchar& codeword) {
+	rv<VecUchar> decode(const VecUchar& codeword) {
         if(codeword.size() != code_length) {
             throw("cannot decode this!");
         }
-		block->reset(); // not needed?
+		block.reset(); // not needed?
 		for (int i = 0; i < code_length; i++) {
-            block->data[i] = static_cast<galois::field_symbol>(codeword[i]);
+            block.data[i] = static_cast<field_symbol_t>(codeword[i]);
         }
 		decoder.decode(block);
 		VecUchar message(code_length);
-		for (int i = 0; i < code_length; i++) message[i] = static_cast<Uchar>(block->data[i]);
-        rv<VecUchar> ret(block);
-        ret.message(std::move(message));
+		for (int i = 0; i < code_length; i++) message[i] = static_cast<Uchar>(block.data[i]);
+        rv<VecUchar> ret(block,message);
 		return ret;
 	}
 	rv<VecUchar> decode(const VecUchar& codeword,const std::vector<size_t>& erasures) {
         if(codeword.size() != code_length) {
             throw("cannot decode this!");
         }
-		block->reset(); // not needed?
+		block.reset(); // not needed?
 		for (int i = 0; i < code_length; i++) {
-            block->data[i] = static_cast<galois::field_symbol>(codeword[i]);
+            block.data[i] = static_cast<field_symbol_t>(codeword[i]);
         }
 		decoder->decode(block,erasures);
-		rv<VecUchar> ret(block);
         VecUchar message(code_length);
-		for (int i = 0; i < code_length; i++) message[i] = static_cast<Uchar>(block->data[i]);
-        ret.message(std::move(message));
+		for (int i = 0; i < code_length; i++) message[i] = static_cast<Uchar>(block.data[i]);
+		rv<VecUchar> ret(block,message);
 		return ret;
 	}
 	auto decode(b8 codeword) {
-		block->reset(); // not needed?
-		for (size_t i = 0; i < code_length; i++) {
-            block->data[i] = static_cast<galois::field_symbol>(codeword.b[i]);
+		block.reset(); // not needed?
+		for (size_t i = 0; i < code_pad; i++) {
+            block.data[i] = static_cast<field_symbol_t>(codeword.b[i]);
         }
-		decoder->decode(*block);
-        std::cout << "actual decode finished\n";
-		b8 message(*(unsigned long long *)(block->data));
-        std::cout << "b8 message is: " << message.w << '\n';
-        rv<b8> ret(block);
-        ret.message = message;
+		decoder->decode(block);
+		b8 message(*(unsigned long long *)(block.data));
+        rv<b8> ret(block,message);
 		return ret;
 	}
 	auto decode(const b8 codeword,const std::vector<size_t>& erasures) {
-		block->reset(); // not needed?
-		for (int i = 0; i < code_length; i++){
-            block->data[i] = static_cast<galois::field_symbol>(codeword.b[i]);
+		block.reset(); // not needed?
+		for (size_t i = 0; i < code_pad; i++){
+            block.data[i] = static_cast<field_symbol_t>(codeword.b[i]);
         }
-		decoder->decode(*block,erasures);
-		b8 message(*(unsigned long long *)(block->data));
-        std::cout << "b8 message is: " << message.w << '\n';
-        rv<b8> ret(block);
-        ret.message = message;
+		decoder->decode(block,erasures);
+		b8 message(*(unsigned long long *)(block.data));
+        rv<b8> ret(block,message);
 		return ret;
 	}
 
